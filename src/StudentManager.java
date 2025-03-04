d07e4678770c40779c80b9e63708c6f90dd0ae81
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-04 22:58:02 +0530

    Add StudentManager class for managing student records

---
+++ src/StudentManager.java
@@ -0,0 +1,9 @@
+// StudentManager.java
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentManager {
+    private ArrayList<Student> students; // Dynamic storage for students
+    private Scanner scanner;
+}
