0441dff6811eb556e2dbe927aba8f3161a91d411
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-04 23:14:53 +0530

    Add Main class to initialize StudentManager and set up user input

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Name: Samarth Sandesh Bhadane
+// PRN: 24070126503
+// Batch: A2    
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StudentManager manager = new StudentManager();
+        Scanner scanner = new Scanner(System.in);
+    }
+}
