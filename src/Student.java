a83046c11db88d7688438903c0bc7883601d711a
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-04 22:56:16 +0530

    Add constructor to Student class with attributes

--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,18 @@
 // Student.java
+// Defines a Student class with attributes and getters/setters
 
 public class Student {
     private String prn;   // 11-digit PRN
     private String name;
     private String dob;   // Date of Birth (DD/MM/YYYY)
     private double marks;
+
+    // Constructor
+    public Student(String prn, String name, String dob, double marks) {
+        this.prn = prn;
+        this.name = name;
+        this.dob = dob;
+        this.marks = marks;
+    }
+
 }
