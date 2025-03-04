0f9e2ca14a59637bb47852affded8a6ca0bcab84
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-04 23:15:22 +0530

    Add menu options for student management in Main class

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,17 @@ public class Main {
 
         StudentManager manager = new StudentManager();
         Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search Student by PRN");
+            System.out.println("4. Search Student by Name");
+            System.out.println("5. Update Student");
+            System.out.println("6. Delete Student");
+            System.out.println("7. Exit");
+            System.out.print("Enter choice: ");
+        }
     }
 }
