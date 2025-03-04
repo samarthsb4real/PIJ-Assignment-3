017298bb2054c318c1817d18c832e0aaa326091a
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-04 22:56:00 +0530

    Add Student class with basic attributes

---
+++ src/Student.java
@@ -0,0 +1,8 @@
+// Student.java
+
+public class Student {
+    private String prn;   // 11-digit PRN
+    private String name;
+    private String dob;   // Date of Birth (DD/MM/YYYY)
+    private double marks;
+}
